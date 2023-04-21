48f159309ea86a6ffbc59857ec31f9a292bf7ceb
Edem K <kimalidinov@list.ru>
2023-04-21 14:57:01 +0300

    Modify programm

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат.
+//Например 4 > 16.
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // "7" > 7
+int square = number * number;
+Console.Write($"Квадрат числа:  {square}");
