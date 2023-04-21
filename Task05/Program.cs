67a74fdf662969d3baf594f049512089424e62b2
Edem K <kimalidinov@list.ru>
2023-04-21 15:05:08 +0300

    add task05

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Написать программу, которая на вход принимает одно целое число 
+(N), на выходе выводит все цисла от -N до N*/
+Console.WriteLine("Введите целое положитльное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
