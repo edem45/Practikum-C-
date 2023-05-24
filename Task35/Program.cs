41e5981288c74b6a3306ab993751f7e1107dd96e
Edem K <kimalidinov@list.ru>
2023-05-24 19:14:45 +0300

    add task 35

---
+++ Task35/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+int[] CreateArrayRNDInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min,max+1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length -1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountElemInRange (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100 ) count ++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите кол-во символов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число диапазона: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRNDInt(size,min,max);
+Console.Write("[");
+PrintArray(array,",");
+Console.Write("] -->");
+Console.Write(CountElemInRange(array));
