ac0b64a58910cb246c24877a6556583fe06e80fd
Edem K <kimalidinov@list.ru>
2023-05-30 00:16:35 +0300

    add task 51

---
+++ Task51/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 51: Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],10} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int Diogonal(int[,] matrix)
+{
+    int sum = 0;
+    int size = matrix.GetLength(0) > matrix.GetLength(1) ? matrix.GetLength(1) : matrix.GetLength(0);
+    for (int i = 0; i < size; i++)
+    {
+        sum += matrix[i,i];
+    }
+
+    return sum;
+}
+
+int[,] arra2s = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(arra2s);
+int a = Diogonal(arra2s);
+Console.WriteLine();
+Console.WriteLine(a);
