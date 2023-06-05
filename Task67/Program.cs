1cc80d86cab05930ee25be9a0392b1401500870f
Edem K <kimalidinov@list.ru>
2023-06-06 00:11:26 +0300

    add task 67

---
+++ Task67/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigitsInNumberRec = SumDigitsInNumberRec(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {sumDigitsInNumberRec}");
+
+
+
+int SumDigitsInNumberRec(int n)
+ {
+     if (n == 0) return 0;
+     return n % 10 + SumDigitsInNumberRec(n/10); // 453 45 4
+ }
