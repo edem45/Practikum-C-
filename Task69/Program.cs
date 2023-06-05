cb0776ea94b4a64b19f9099008031139e71df44a
Edem K <kimalidinov@list.ru>
2023-06-06 00:11:44 +0300

    add task 69

---
+++ Task69/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Введите целое число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+int degree = Degree(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {degree}");
+
+
+
+/*
+int Degree(int numA, int numB)
+{
+     if (numB == 0) return 1;
+     return numA * Degree(numA, numB-1); //A 3 3 3 3 3   B 5 4 3 2 1
+}
+*/
+
+ int Degree(int numA, int numB)
+ {
+    return numB == 0 ? 1 : numA * Degree(numA, numB-1);
+ }
