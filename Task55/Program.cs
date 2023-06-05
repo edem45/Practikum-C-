e1ba53b7ad249877935eb39d1db18af6fb27462f
Edem K <kimalidinov@list.ru>
2023-06-06 00:09:16 +0300

    add task 55

---
+++ Task55/Program.cs
@@ -0,0 +1,65 @@
+﻿/*// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.*/
+
+
+int rows = 3;
+int columns = 3;
+if (rows != columns)
+{
+    Console.WriteLine("");
+    return;
+}
+
+int[,] array2d = CreateMatrixRndInt(rows, columns, 0, 100);
+PrintMatrix(array2d);
+Replace(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write($"{matrix[i, j],5} ");
+
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void Replace(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+
+        }
+    }
+}
