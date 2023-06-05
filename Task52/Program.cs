7daa781355a3d3f2577d5cdfde14b1fb5d196af4
Edem K <kimalidinov@list.ru>
2023-06-06 00:08:37 +0300

    modify task 52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -5,8 +5,14 @@
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
-
-int[,] array2d = CreateMatrixRndInt(4, 5, 0, 10);
+int rows = 6;
+int columns = 6;
+int[,] array2d = CreateMatrixRndInt(rows, columns, 0, 100);
+//double[] columsnum = ColumnsNum(columns);
+//PrintArray(columsnum);
+//Console.WriteLine();
+//Line("_____", columns);
+//Console.WriteLine();
 PrintMatrix(array2d);
 Console.WriteLine();
 double[] array = SumColumnsMatrix(array2d);
@@ -29,6 +35,24 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     return matrix;
 }
 
+/*double[] ColumnsNum (int size)
+{
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
+*/
+/*void Line (string a, int size)
+{
+   for (int i = 0; i < size; i++)
+   {
+     Console.Write("_", 5);
+   }
+}
+*/
 double[] SumColumnsMatrix(int[,] matrix)
 {
     double sum = 0;
@@ -39,7 +63,7 @@ double[] SumColumnsMatrix(int[,] matrix)
         {
             sum += matrix[i, j];
         }
-        arr[j] = sum/matrix.GetLength(0);
+        arr[j] = Math.Round(sum/matrix.GetLength(0), 1);
         sum = 0;
     }
 
@@ -47,12 +71,12 @@ double[] SumColumnsMatrix(int[,] matrix)
 
 }
 
-void PrintArray(double[] arr, string sep = ",")
+void PrintArray(double[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
-        else Console.Write($"{arr[i]} ");
+        if (i < arr.Length - 1) Console.Write($"{arr[i], 5} ");
+        else Console.Write($"{arr[i],5} ");
     }
 }
 
@@ -62,9 +86,11 @@ void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        //Console.Write(i + "|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j],5} ");
+            
+            Console.Write($"{matrix[i, j], 5} ");
 
         }
         
